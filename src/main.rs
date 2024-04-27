14054487eff6e30d733f051d34f635117ad55211
GlennOnyango <glennonyango@yahoo.com>
2024-04-27 14:51:01 +0300

    Code organisation completed

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,21 @@
 use std::env;
-use std::fs;
+use std::process;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Search for {}", config.query);
 
     println!("In {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path).expect("Should be able to read file");
-
-    println!("The contents of the file\n{contents}");
+    if let Err(e) = minigrep::run(config){
+        println!("Application error:{e}");
+        process::exit(1);
+    };
 }
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config{
-
-    fn new(args: &[String]) -> Config {
-
-        if args.len() < 3{
-            panic!("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-    
-        Config { query, file_path }
-    }
-}
-
-
